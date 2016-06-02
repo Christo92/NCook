aa234c0728ad9fca23eda34fdcade74ed3c8c66f
Christo92 <christpher.loubassou@hotmail.fr>
2016-06-02 11:42:15 +0200

    azgrt

--- sign-up.php
+++ sign-up.php
@@ -23,7 +23,7 @@
 			 	{
 					 ?>
                      <div class="alert alert-danger">
-                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $errors; ?>
                      </div>
                      <?php
 				}
